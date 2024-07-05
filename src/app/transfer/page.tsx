3373476cc1f13e2f6673662e7bf6f8c08b3530e9
harshit340 <harshitshrivastava340@gmail.com>
2024-07-05 21:24:20 +0530

    fixed some error

--- src/app/transfer/page.tsx
+++ src/app/transfer/page.tsx
@@ -34,26 +34,11 @@ export default function page() {
 
   return (
     <div>
-      <main className="flex items-center justify-center min-h-screen">
-                <div className="border hover:border-slate-900 rounded p-4">
-                    <h1>This is the transfer Page</h1>
-                    <input
-                        type="text"
-                        placeholder="Recipient Public Key"
-                        value={receipientKey}
-                        onChange={(e) => setReceipientKey(e.target.value)}
-                        className="border rounded p-2 mt-4"
-                    />
-                    <button onClick={sendSols} className="ml-4 p-2 bg-blue-500 text-white rounded">
-                        Send 1 LAMPORT
-                    </button>
-                    {signature && (
-                        <div className="mt-4">
-                            <h2>Transaction Signature: {signature}</h2>
-                        </div>
-                    )}
-                </div>
-            </main>
+           <input type="text"  placeholder="Recipient Public Key" value={receipientKey} onChange={(e) => setReceipientKey(e.target.value)}/>    
+   <button onClick={sendSols} >Send</button>
+                    
+                
+            
     </div>
   )
 }
